d542a83e11f6439952183fb5751d50fe4d285dcc
Emanuel Sanga <esanga530@gmail.com>
2019-09-12 15:27:53 +0300

    modified settings to include rest framework settings

--- jwtoly/settings.py
+++ jwtoly/settings.py
@@ -92,6 +92,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'authentication.backends.JWTAuthentication',
+        )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
