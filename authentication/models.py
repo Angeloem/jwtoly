b467891fd1c3cfafe1356199db9dd7c5f8f352be
Emanuel Sanga <esanga530@gmail.com>
2019-09-12 15:01:08 +0300

    added models user and user-manager

---
+++ authentication/models.py
@@ -0,0 +1,145 @@
+from django.db import models
+from django.conf import settings
+from django.core.validators import (validate_email, RegexValidator)
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, BaseUserManager)
+from django.utils import timezone
+
+import jwt
+
+
+class UserManager(BaseUserManager):
+    """
+    Django requires that custom users define their own Manager class. By
+    inheriting from `BaseUserManager`, we get a lot of the same code used by
+    Django to create a `User`.
+
+    All we have to do is override the `create_user` function which we will use
+    to create `User` objects.
+    """
+
+    def _create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The given username must be set')
+
+        if not email:
+            raise ValueError('The given email must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        """
+        Create and return a `User` with an email, username and password.
+        """
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        """
+        Create and return a `User` with superuser (admin) permissions.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Defines our custom user class.
+    Username, email and password are required.
+    """
+
+    username = models.CharField(
+        validators=[
+            RegexValidator(
+                regex=r"[\w]{6,25}",
+                message=
+                'Usernames must be between 6 and 25 characters long and can only contain letters, numbers, and underscores.'
+            )
+        ],
+        db_index=True,
+        max_length=25,
+        unique=True,
+    )
+
+    email = models.EmailField(validators=[validate_email],
+                              max_length=128,
+                              unique=True)
+
+    is_staff = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+
+    # The `USERNAME_FIELD` property tells us which field we will use to log in.
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+    # Tells Django that the UserManager class defined above should manage
+    # objects of this type.
+    objects = UserManager()
+
+    def __str__(self):
+        """
+        Returns a string representation of this `User`.
+        This string is used when a `User` is printed in the console.
+        """
+        return self.username
+
+    @property
+    def token(self):
+        """
+        Allows us to get a user's token by calling `user.token` instead of
+        `user.generate_jwt_token().
+
+        The `@property` decorator above makes this possible. `token` is called
+        a "dynamic property".
+        """
+        return self._generate_jwt_token()
+
+    def get_full_name(self):
+        """
+        This method is required by Django for things like handling emails.
+        Typically this would be the user's first and last name. Since we do
+        not store the user's real name, we return their username instead.
+        """
+        return self.username
+
+    def get_short_name(self):
+        """
+        This method is required by Django for things like handling emails.
+        Typically, this would be the user's first name. Since we do not store
+        the user's real name, we return their username instead.
+        """
+        return self.username
+
+    def _generate_jwt_token(self):
+        """
+        Generates a JSON Web Token that stores this user's ID and has an expiry
+        date set to 60 days into the future.
+        """
+        exp = timezone.now() + timezone.timedelta(days=60)
+        iat = timezone.now()
+        token = jwt.encode(
+            {
+                "iat": int(iat.strftime("%s")),
+                "id": self.pk,
+                "exp": int(exp.strftime("%s")),
+            },
+            settings.SECRET_KEY,
+            algorithm="HS256",
+        )
+        return token.decode("utf-8")
