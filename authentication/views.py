8204a763585da4f8f9c0b6470046adfbd4a88b18
Emanuel Sanga <esanga530@gmail.com>
2019-09-12 15:45:06 +0300

    added a userlogin and creation views using viewsets

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .serializers import (LoginSerializer, RegistrationSerializer)
 
-# Create your views here.
+
+class UserViewSet(viewsets.ViewSet):
+    """
+    The `auth` endpoint allows only specific actions including login, registration, etc.
+    """
+
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
+    def login(self, request):
+        """
+        Login existing `User`.
+        """
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
+    def registration(self, request):
+        """
+        Register a new `User` account.
+        """
+        serializer = RegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
